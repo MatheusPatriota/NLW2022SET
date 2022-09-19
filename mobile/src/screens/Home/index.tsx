a157ab38633b0834e47e4c4d5eb246a662e9d822
Matheus Patriota <mpatriota55@gmail.com>
2022-09-18 21:28:42 -0300

    heading component created

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Image, View } from 'react-native';
+import React from "react";
+import { Image, View } from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-import Logo from '../../assets/logo-nlw-esports.png';
+import Logo from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image source={Logo} style={styles.logo}/>
+      <Image source={Logo} style={styles.logo} />
+      <Heading
+        title="Encontre seu Duo"
+        subtitle="Selecione o Jogo que deseja jogar"
+      />
     </View>
   );
 }
